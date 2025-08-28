610eecf4ac8fca752868175df7324054d7e3bd03
Rodney Nedlose <nedloserodney@gmail.com>
2025-08-28 14:21:04 -0500

    First Commit

---
+++ neovim.lua
@@ -0,0 +1,18 @@
+return {
+	{
+		"vague2k/vague.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {
+			style = {
+				background = "light"
+			}
+		}
+	},
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "vague",
+		},
+	},
+}
